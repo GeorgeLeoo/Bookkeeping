5143a42a1f2ffb23fe2b90b064310d1c77ca9599
jxd <18921483103@qq.com>
2021-03-08 19:12:17 +0800

    计算器

--- miniprogram/components/keyboard/keyboard.js
+++ miniprogram/components/keyboard/keyboard.js
@@ -88,7 +88,10 @@ Component({
         value: '',
         type: 'ok'
       },
-    ]
+    ],
+    numberStack: [],
+    operatorStack: [],
+    flg: '',
   },
 
   /**
@@ -96,22 +99,85 @@ Component({
    */
   methods: {
     bindKeyboardTap(e) {
-      const item = e.currentTarget.dataset.item
-      if (item.type === 'ok') {
+      let { type, value } = e.currentTarget.dataset.item
+      if (type === 'ok') {
         this.triggerEvent('ok', {})
         return
       }
-      let formula = this.data.formula + item.value
-      console.log(formula);
-      if (item.type === 'operator') {
-        // formula = eval(formula)
-        const list = ['+', '-']
-        const operator = list.indexOf(item.value)
-        console.log(formula.split(operator));
+      if (type === 'date') {
+        return
       }
-      this.setData({
-        formula
-      })
+      if (type === 'del') {
+        let formula = this.data.formula
+        formula = (formula + '').substring(0, formula.length - 1)
+        this.setData({
+          formula: formula === '' ? 0 : formula
+        })
+      }
+    
+      if (type === 'number') {
+        let numberStack = this.data.numberStack
+
+        let finalFormula = 0
+        let finalValue = ''
+
+        finalFormula = this.data.formula + value + ''
+
+        if (numberStack.length === 0) {
+          numberStack.push(value + '')
+        } else {
+          if (this.data.flg === 'operator') {
+            numberStack.push(value + '')
+          } else {
+            let index = numberStack.length - 1
+            numberStack[index] = numberStack[index] + value + ''
+          }
+        }
+        
+        this.setData({
+          formula: finalFormula,
+          numberStack,
+          flg: type
+        })
+      }
+      if (type === 'operator') {
+        let numberStack = this.data.numberStack
+        let operatorStack = this.data.operatorStack
+        let finalFormula = 0
+        if (numberStack.length === 2 && operatorStack.length === 1) {
+          let numberStack0 = Number(numberStack.pop())
+          let operatorStack0 = operatorStack.pop()
+          let numberStack1 = Number(numberStack.pop())
+          
+          if (operatorStack0 === '+') {
+            finalFormula = numberStack0 + numberStack1
+          }
+          if (operatorStack0 === '-') {
+            finalFormula = numberStack0 - numberStack1
+          }
+          numberStack.push(finalFormula)
+          finalFormula = finalFormula + value
+        } else {
+          finalFormula = this.data.formula + value
+        }
+        operatorStack.push(value)
+        this.setData({
+          formula: finalFormula,
+          operatorStack,
+          flg: type
+        })
+      }
+      console.log(this.data);
+      // let formula = this.data.formula + value
+      // if (item.type === 'operator') {
+      //   // formula = eval(formula)
+      //   const list = ['+', '-']
+      //   const operator = list.indexOf(item.value)
+      //   console.log(formula.split(operator));
+      // }
+      // this.setData({
+      //   formula
+      // })
     },
     bindDateChange(e) {
       this.setData({
